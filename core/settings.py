dbf7ed80d757e9db52d53b921c6b5cf1d49b4823
Fanpool <fanpool@icloud.com>
2024-09-16 13:54:26 +0300

    0.04

--- core/settings.py
+++ core/settings.py
@@ -93,6 +93,10 @@ timezone.activate(pytz.timezone(TIME_ZONE))
 timezone.localtime(timezone.now())
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
