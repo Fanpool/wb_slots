b32930828299e690422b570e2ab5c48a2330996f
Fanpool <fanpool@icloud.com>
2024-09-17 20:31:51 +0300

    0.19

--- core/celery.py
+++ core/celery.py
@@ -7,7 +7,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", 'core.settings')
 app = Celery('wb_slots')
 app.config_from_object('django.conf:settings', namespace="CELERY")
 app.autodiscover_tasks()
-
+app.control.purge()
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
